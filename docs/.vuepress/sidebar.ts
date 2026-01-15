9cf982c3e685dcc0404e846debdff974541aae87
Macros Meng <Meng-yc@outlook.com>
2026-01-15 20:41:21 +0800

    docs: 快速开始(P1)

--- docs/.vuepress/sidebar.ts
+++ docs/.vuepress/sidebar.ts
@@ -7,7 +7,7 @@ export default sidebar({
       text: '使用文档',
       icon: 'book-open',
       prefix: 'usage/',
-      children: ['']
+      children: ['quickstart']
     },
     {
       text: '插件开发',
