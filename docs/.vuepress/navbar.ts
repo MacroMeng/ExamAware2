ca6b5d4be4a41075fd43a83bb6433d40a272d97b
Macros Meng <Meng-yc@outlook.com>
2026-01-18 17:11:24 +0800

    docs: 更新侧边栏/导航栏

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -9,7 +9,16 @@ export default navbar([
   {
     text: '使用文档',
     link: '/usage/',
-    icon: 'book-open'
+    icon: 'book-open',
+    children: [
+      { text: '使用文档', link: '/usage/' },
+      { text: '快速开始', link: '/usage/quickstart.md' },
+      { text: '编辑器', link: '/usage/feat/editor.md' },
+      { text: '放映器', link: '/usage/feat/player.md' },
+      { text: '共享与投送', link: '/usage/feat/lan_share.md' },
+      { text: '设置界面', link: '/usage/feat/settings.md' },
+      { text: '日志', link: '/usage/feat/logging.md' }
+    ]
   },
   {
     text: '插件开发',
@@ -21,6 +30,11 @@ export default navbar([
       { text: '打包与分发', link: 'packaging' },
       { text: 'API 概览', link: 'api' }
     ]
+  },
+  {
+    text: '常见问题/报告问题',
+    link: 'usage/faq_and_report.md',
+    icon: 'question'
   },
   {
     text: 'GitHub',
--- docs/.vuepress/sidebar.ts
+++ docs/.vuepress/sidebar.ts
@@ -7,13 +7,25 @@ export default sidebar({
       text: '使用文档',
       icon: 'book-open',
       prefix: 'usage/',
-      children: ['quickstart']
+      children: [
+        'quickstart',
+        'feat/editor.md',
+        'feat/player.md',
+        'feat/lan_share.md',
+        'feat/settings.md',
+        'feat/logging.md'
+      ]
     },
     {
       text: '插件开发',
       icon: 'puzzle-piece',
       prefix: 'plugin-dev/',
       children: ['', 'quickstart', 'packaging', 'api']
+    },
+    {
+      text: '常见问题/报告问题',
+      link: 'usage/faq_and_report.md',
+      icon: 'question'
     }
   ]
 });
